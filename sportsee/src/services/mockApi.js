ba2fa380d6f82510e807817711af1a5fb2d910f1
IsmaSEO <ismail.jelassi.dev@gmail.com>
2025-01-15 10:06:01 +0100

    ajout de la gestion des mocks et enrichissement des données dans mockApi.js

--- sportsee/src/services/mockApi.js
+++ sportsee/src/services/mockApi.js
@@ -1,4 +1,5 @@
 // src/services/mockApi.js
+import { USE_MOCKS } from "../config"; // Import de la configuration
 
 // J'ai défini ici un tableau contenant des données simulées pour les utilisateurs.
 const users = [
@@ -25,13 +26,13 @@ const users = [
       { value: 90, kind: "intensity" },
     ],
     sessions: [
-      { day: 1, sessionLength: 30 },
-      { day: 2, sessionLength: 40 },
-      { day: 3, sessionLength: 50 },
-      { day: 4, sessionLength: 30 },
-      { day: 5, sessionLength: 30 },
-      { day: 6, sessionLength: 50 },
-      { day: 7, sessionLength: 50 },
+      { day: 1, sessionLength: 30, kilogram: 80, calories: 240 },
+      { day: 2, sessionLength: 40, kilogram: 80, calories: 220 },
+      { day: 3, sessionLength: 50, kilogram: 81, calories: 280 },
+      { day: 4, sessionLength: 30, kilogram: 81, calories: 290 },
+      { day: 5, sessionLength: 30, kilogram: 80, calories: 160 },
+      { day: 6, sessionLength: 50, kilogram: 78, calories: 162 },
+      { day: 7, sessionLength: 50, kilogram: 76, calories: 390 },
     ],
   },
   {
@@ -57,19 +58,24 @@ const users = [
       { value: 75, kind: "intensity" },
     ],
     sessions: [
-      { day: 1, sessionLength: 45 },
-      { day: 2, sessionLength: 60 },
-      { day: 3, sessionLength: 40 },
-      { day: 4, sessionLength: 30 },
-      { day: 5, sessionLength: 60 },
-      { day: 6, sessionLength: 70 },
-      { day: 7, sessionLength: 40 },
+      { day: 1, sessionLength: 45, kilogram: 70, calories: 300 },
+      { day: 2, sessionLength: 60, kilogram: 70, calories: 240 },
+      { day: 3, sessionLength: 40, kilogram: 70, calories: 220 },
+      { day: 4, sessionLength: 30, kilogram: 70, calories: 280 },
+      { day: 5, sessionLength: 60, kilogram: 70, calories: 500 },
+      { day: 6, sessionLength: 70, kilogram: 69, calories: 160 },
+      { day: 7, sessionLength: 40, kilogram: 69, calories: 162 },
     ],
   },
 ];
 
 // J'ai créé une fonction qui simule un appel API pour récupérer les données d'un utilisateur en fonction de son ID.
 export const getUserData = (id) => {
+  if (!USE_MOCKS) {
+    throw new Error(
+      "Les mocks sont désactivés. Activez USE_MOCKS dans la configuration."
+    );
+  }
   return new Promise((resolve, reject) => {
     const user = users.find((u) => u.id === id);
     setTimeout(() => {
